ca72c1736ceb32f4015940111f8a50aa8d7e787b
Moyses Dutra <moysesdutra@gmail.com>
2022-10-09 14:29:41 -0300

    Nova funcionalidade  - Redemoinho

--- image_processing/processing/transformation.py
+++ image_processing/processing/transformation.py
@@ -1,4 +1,5 @@
 from skimage.transform import resize
+from skimage.transform import swirl
 
 def resize_image(image, proportion):
     assert 0 <= proportion <=1, "Specify a valid proportion between 0 and 1."
@@ -6,3 +7,8 @@ def resize_image(image, proportion):
     width = round(image.shape[1] * proportion)
     image_resized = resize(image, (height, width), anti_aliasing=True)
     return image_resized
+
+
+def redemoinho(image, rotacao, forca, raio):
+    assert 0 <= rotacao <= 360,  "Specify a valid rotation between 0 and 360º."
+    return swirl(image, rotacao, forca, raio)
